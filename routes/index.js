e01d0b32bc93019a4904dfc694afe8eabf142017
militarpancho <militarpancho_93@msn.com>
2015-05-18 16:33:22 +0200

    Moderación de comentarios

--- routes/index.js
+++ routes/index.js
@@ -34,7 +34,7 @@ router.delete('/quizes/:quizId(\\d+)',     sessionController.loginRequired, quiz
 router.get('/quizes/:quizId(\\d+)/comments/new',            commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments',              commentController.create);
 router.get('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish', 
-+	                                    sessionController.loginRequired, commentController.publish);
+	                                    sessionController.loginRequired, commentController.publish);
 
 
 router.get('/author',function(req, res){
