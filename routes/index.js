083478e4b6872c01b6d30663ca713078782983b2
militarpancho <militarpancho_93@msn.com>
2015-05-18 13:33:04 +0200

    Autenticación y sesión

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller');
 var commentController = require('../controllers/comment_controller');
+var sessionController = require('../controllers/session_controller');
 
 // Página de entrada
 router.get('/', function(req, res) {
@@ -12,6 +13,11 @@ router.get('/', function(req, res) {
 
 // Autoload de comandos con :quizId
 router.param('quizId', quizController.load); //autoload :quizId
+
+// Definición de rutas de sesion
++router.get('/login',  sessionController.new);     // formulario login
++router.post('/login', sessionController.create);  // crear sesión
++router.get('/logout', sessionController.destroy); // destruir sesión
 
 
 // Definición de rutas de /quizes
